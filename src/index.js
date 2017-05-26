c2f275da3ef31b5793068581a4262052d50bc8a7
JayBauer <jay.bauer@hotmail.com>
2017-05-26 00:30:10 -0400

    Section 1 Complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 
-const App = () => <div>Hi!</div>;
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBtbpsJ_Qh4fTKRBA-5tmc-vhZhQeIxU5A';
+
+const App = () => <div><SearchBar /></div>;
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-=======
-
-const App = function() {
-  return <div>Test</div>
-}
->>>>>>> 4f46bd7082245e14230cf04d83b79d9161713e26
